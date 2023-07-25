2643f8fd19561ecebcc76b870b5d614eaebf1ded
Mr. R <roberto.recopelacion@gmail.com>
2023-07-25 18:53:49 +0800

    fix mobile view header and footer

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,5 +1,11 @@
 (function($){
 
-    
+    // hamburger menu
+    $(".sf_main-header--hamburger").on('click', function(e) {
+        e.preventDefault();
+        $(this).toggleClass('openMenu')
+        $('.sf_main-header--menu').toggleClass('openNav')
+        $('body').toggleClass('openNav')
+    })
 
 })(jQuery);
